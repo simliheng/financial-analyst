05c7cfe86c42cca10653e7884f140bde1ac938a3
liheng <lyhengsim1@gmail.com>
2025-07-18 16:59:18 +0700

    del desk

--- Desktop/financail analyst/api/serializers.py
+++
@@ -1,9 +0,0 @@
-# backend/api/serializers.py
-
-from rest_framework import serializers
-from users.models import CustomUser
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'is_admin', 'is_user']
